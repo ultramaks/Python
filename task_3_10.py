b8aa9a1603b9979c2e3fc22a73710518940d882c
Maxim Filippov <Maxim.Filippov1@ibm.com>
2020-06-11 17:44:38 +0300

    Initial commit

---
+++ task_3_10.py
@@ -0,0 +1,16 @@
+import random
+
+values = []
+
+size = int(input("Введите размер списка:\n"))
+for i in range(size):
+    x = random.randint(0, 99)
+    values.append(x)
+
+print("Список - " + str(values))
+
+sum = values[-1] + values[-2] + values[-3]
+sr_ar = sum/3
+
+print ("Сумма трех последних чисел = " + str(sum))
+print ("Среднее арифметическое = " + str(sr_ar))
